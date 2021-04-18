8e6b373c3d68bded01c813ecd6b8ae5209217854
Sofiane BELAZOUZ <sofianebelazouz@gmail.com>
2021-04-18 15:39:44 +0200

    Fixed test to order bills by date

--- babel.config.cjs
+++ babel.config.cjs
@@ -1,12 +1,13 @@
 module.exports = {
+  plugins: ["@babel/plugin-proposal-class-properties"],
   presets: [
     [
-      '@babel/preset-env',
+      "@babel/preset-env",
       {
         targets: {
-          node: 'current',
+          node: "current",
         },
       },
     ],
   ],
-}
+};
