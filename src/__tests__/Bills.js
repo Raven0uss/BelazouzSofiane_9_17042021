5af363f14ef11373eaa87d41871f34edfe096863
Sofiane BELAZOUZ <sofianebelazouz@gmail.com>
2021-04-19 18:18:42 +0200

    Added GET integration's tests and covered Bills container

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -1,27 +1,17 @@
-import { screen } from "@testing-library/dom";
+import { fireEvent, screen } from "@testing-library/dom";
 import BillsUI from "../views/BillsUI.js";
 import { localStorageMock } from "../__mocks__/localStorage.js";
 import { bills } from "../fixtures/bills.js";
+import Bills from "../containers/Bills.js";
+import firebase from "../__mocks__/firebase";
+import { ROUTES, ROUTES_PATH } from "../constants/routes";
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on Bills Page", () => {
     test("Then bill icon in vertical layout should be highlighted", () => {
-      Object.defineProperty(window, "localStorage", {
-        value: localStorageMock,
-      });
-      window.localStorage.setItem(
-        "user",
-        JSON.stringify({
-          type: "Employee",
-          status: "connected",
-        })
-      );
-
       const html = BillsUI({ data: [] });
       document.body.innerHTML = html;
-
-      const billIcon = screen.getByTestId("icon-window");
-      expect(billIcon).toBeTruthy();
+      //to-do write expect expression
     });
     test("Then bills should be ordered from earliest to latest", () => {
       const html = BillsUI({ data: bills });
@@ -44,6 +34,76 @@ describe("Given I am connected as an employee", () => {
       const html = BillsUI({ error: true });
       document.body.innerHTML = html;
       expect(screen.getAllByText("Erreur")).toBeTruthy();
+    });
+
+    test("fetches bills from mock API GET", async () => {
+      const getSpy = jest.spyOn(firebase, "get");
+      const bills = await firebase.get();
+      expect(getSpy).toHaveBeenCalledTimes(1);
+      expect(bills.data.length).toBe(4);
+    });
+    test("fetches bills from an API and fails with 404 message error", async () => {
+      firebase.get.mockImplementationOnce(() =>
+        Promise.reject(new Error("Erreur 404"))
+      );
+      const html = BillsUI({ error: "Erreur 404" });
+      document.body.innerHTML = html;
+      const message = await screen.getByText(/Erreur 404/);
+      expect(message).toBeTruthy();
+    });
+    test("fetches messages from an API and fails with 500 message error", async () => {
+      firebase.get.mockImplementationOnce(() =>
+        Promise.reject(new Error("Erreur 500"))
+      );
+      const html = BillsUI({ error: "Erreur 500" });
+      document.body.innerHTML = html;
+      const message = await screen.getByText(/Erreur 500/);
+      expect(message).toBeTruthy();
+    });
+  });
+
+  describe("When I click on new bill button", () => {
+    test("Then it should call the function to navigate to NewBill path", () => {
+      document.body.innerHTML = BillsUI({ data: [] });
+
+      const onNavigate = jest.fn((pathname) => {
+        document.body.innerHTML = ROUTES({ pathname });
+      });
+
+      const billsContainer = new Bills({
+        document,
+        localStorage: window.localStorage,
+        onNavigate,
+        PREVIOUS_LOCATION: "",
+        firebase: jest.fn(),
+      });
+
+      const buttonNewBill = screen.queryByTestId("btn-new-bill");
+      fireEvent.click(buttonNewBill);
+      expect(onNavigate).toHaveBeenCalled();
+      expect(onNavigate).toHaveBeenCalledWith(ROUTES_PATH["NewBill"]);
+    });
+  });
+
+  describe("When I click on eyes icon", () => {
+    test("Then a modal should be visible", () => {
+      const html = BillsUI({ data: bills });
+      document.body.innerHTML = html;
+
+      const billsContainer = new Bills({
+        document,
+        localStorage: window.localStorage,
+        onNavigate: jest.fn(),
+        PREVIOUS_LOCATION: "",
+        firebase: jest.fn(),
+      });
+
+      $.fn.modal = jest.fn();
+
+      const eyeIcon = screen.queryAllByTestId("icon-eye");
+      fireEvent.click(eyeIcon[0]);
+      expect($.fn.modal).toHaveBeenCalled();
+      expect($.fn.modal).toHaveBeenCalledWith("show");
     });
   });
 });
